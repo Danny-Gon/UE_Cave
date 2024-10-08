8e5df7510fccbcf52a39594252d473825f3152c2
Danny <52361762+Danny-Gon@users.noreply.github.com>
2024-10-08 16:36:30 -0500

    Parte5

---
+++ Source/UE_Cave/UE_CaveGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE_CaveGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUE_CaveGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUE_CaveGameMode();
+};
+
+
+
